9a4d76e824cd826b808fde4250e226dc42db816c
Amin Norouzi <aminnorouzidev@gmail.com>
2022-05-30 03:10:31 +0430

    Created scraper service

---
+++ scraper-service/src/main/java/com/aminnorouzi/scraperservice/ScraperServiceApplication.java
@@ -0,0 +1,15 @@
+package com.aminnorouzi.scraperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ScraperServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScraperServiceApplication.class, args);
+    }
+
+}
